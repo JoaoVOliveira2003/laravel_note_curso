ae21da2adfd4c12283efea6e2e69f579ef54633c
JoaoVOliveira2003 <ojoao953@gmail.com>
2025-12-04 18:48:40 -0300

    .

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
+//aqui coloca o caminhi das paginas basicamente!.
+/*
+primeira parte é a rota, segunda parte é o que vai acontecer quando acessar essa rota
+  1 parte: rota
+  2 parte: ação
+  3 parte: nome da rota (opcional)
+*/
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
-
+// leva até C:\laragon\www\notes\resou=,rces\views\welcome.blade.php
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/olaMundo', function () {
+     echo "Olá Mundo!";
+});
+
+//aqui executa a classe lá daquele main
+Route::get('/main',[MainController::class,'index']);
